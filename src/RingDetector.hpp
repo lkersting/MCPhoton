4b8d947bfa223eafb81b2c6265954ff8255f3c55
Luke <lkersting@wisc.edu>
2014-07-10 15:53:35 -0500

    last commit

---
+++ src/RingDetector.hpp
@@ -0,0 +1,23 @@
+//---------------------------------------------------------------------------//
+//!
+//! \file   RingDetector.hpp
+//! \author Luke Kersting
+//! \brief  Calculate particles crossing plane within a circular area 
+//!
+//---------------------------------------------------------------------------//
+
+#ifndef RING_DETECTOR_HPP
+#define RING_DETECTOR_HPP
+
+void Ring ( std::string data,
+			double (&radius),
+			int (&Transmitted),
+			int (&N)			);
+								
+
+#endif // end RING_DETECTOR_HPP
+
+//---------------------------------------------------------------------------//
+// end RingDetector.hpp
+//---------------------------------------------------------------------------//
+
